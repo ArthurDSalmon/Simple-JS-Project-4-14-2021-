1db51fb77bea5dacf49742953b0929d50b1bf2ff
ArthurDSalmon <77698204+ArthurDSalmon@users.noreply.github.com>
2021-04-15 08:43:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
 function upDate(boxColor){
-    document.getElementById('main').style.backgroundImage = "url('')";
-    document.getElementById('main').style.backgroundColor = boxColor.style.backgroundColor;
-}
-
-function upDateTwo(boxColor){
-
-    document.getElementById('main').style.backgroundImage = boxColor.style.backgroundImage;
-}
-
-function upDateTre(boxColor){
-
-    document.getElementById('main').style.backgroundImage = "url("+ boxColor.src +")";
-}
+    document.getElementById('main').className = boxColor.className;}
